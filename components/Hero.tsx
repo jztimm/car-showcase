376b4813f329f603491c49a2508f035b3d84c19d
jztimm <jztimm@gmail.com>
2023-07-25 18:54:35 -0400

    Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { CustomButton } from ".";
+
+const Hero = () => {
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="felx-1 pt-36 paddic-x">
+        <h1 className="hero__title">
+          Find, Book, or rent a car — quickly and easily!
+        </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our efforless booking
+          process
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue
+          text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
